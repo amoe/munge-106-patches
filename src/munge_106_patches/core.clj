382355ac069b787c5619005c3aa1fd948fd7a8f8
David Banks <david.banks@icrossing.co.uk>
2014-12-22 17:26:32 +0000

    parsing file successfully

--- src/munge_106_patches/core.clj
+++ src/munge_106_patches/core.clj
@@ -1,7 +1,45 @@
 (ns munge-106-patches.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn read-dump [dump-path]
+  (with-open [r (io/reader dump-path)]
+    (doall (line-seq r))))
+
+(defn get-indices [dump]
+  (keep-indexed #(when (re-matches #"\[instance 0x.{6}: 0x.{6}/Patch"
+                                   %2)
+                   %1)
+                dump))
+
+(defn munge-sysex-list [list-spec]
+  (map read-string (str/split list-spec #", ")))
+
+(defn parse-definition [definition-line]
+  (let [[match group1] (re-find #"\[arraycoll sz 18 (.*)\]$" definition-line)]
+    (munge-sysex-list group1)))
+
+(defn parse-name [name-line]
+  (let [[match group1] (re-find #"\"(.*)\"" name-line)]
+    group1))
+
+  
+(defn  handle-patch [index dump]
+  (let [patch-definition-line (nth dump (+ index 3))
+        patch-name-line (nth dump (+ index 4))]
+    {:definition (parse-definition patch-definition-line)
+     :name (parse-name patch-name-line)}))
+
+    
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [dump (read-dump (first args))]
+    (map #(handle-patch % dump) (get-indices dump))))
+
+
+
+
