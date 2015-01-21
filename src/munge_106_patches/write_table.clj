8308370fcbcc4202b96e7f70c8575b16a2cbe204
David Banks <david.banks@icrossing.co.uk>
2015-01-21 13:32:15 +0000

    fill template vars

--- src/munge_106_patches/write_table.clj
+++ src/munge_106_patches/write_table.clj
@@ -5,12 +5,74 @@
             [clojure.pprint :as pprint]
             [net.cgrand.enlive-html :as html]))
 
+(defn format-numeric [patch key]
+  (format "%.2f" (key (:definition patch))))
+
+(defn format-boolean [patch key]
+  (if (key (:definition patch))
+    "&#x2713;"
+    "&#x2717;"))
+
+
 (html/deftemplate table-page "table.html" [table]
-  [:table.mylist :tr.patch]  (html/clone-for [patch table]
-                               [:td.name]  (html/content (:name patch))
-                               [:td.lfo-delay-time]
-                               (html/content (format "%.2f"
-                                                     (:lfo-delay-time (:definition patch))))))
+  [:table.mylist :tr.patch]
+  (html/clone-for [patch table]
+                  [:td.name]  (html/content (:name patch))
+                  [:td.lfo-delay-time]
+                  (html/content (format-numeric patch :lfo-delay-time))
+                  [:td.range]
+                  (html/content (:range (:definition patch)))
+                  [:td.dco-lfo]
+                  (html/content (format-numeric patch :dco-lfo))
+
+                  [:td.dco-pwm]
+                  (html/content (format-numeric patch :dco-pwm))
+
+                  [:td.pwm-mode]
+                  (html/content (:pwm-mode (:definition patch)))
+
+                  [:td.pulse?]
+                  (html/html-content (format-boolean patch :pulse?))
+
+                  [:td.tri?]
+                  (html/html-content (format-boolean patch :tri))
+
+                  [:td.dco-sub]
+                  (html/content (format-numeric patch :dco-pwm))
+
+                  [:td.dco-noise]
+                  (html/content (format-numeric patch :dco-noise))
+
+                  [:td.hpf]
+                  (html/content (:hpf (:definition patch)))
+
+                  [:td.vcf-freq]
+                  (html/content (format-numeric patch :vcf-freq))
+                  [:td.vcf-res]
+                  (html/content (format-numeric patch :vcf-res))
+                  [:td.polarity]
+                  (html/content (:polarity (:definition patch)))
+                  [:td.vcf-env]
+                  (html/content (format-numeric patch :vcf-env))
+                  [:td.vcf-lfo]
+                  (html/content (format-numeric patch :vcf-lfo))
+                  [:td.vcf-kybd]
+                  (html/content (format-numeric patch :vcf-kybd))
+
+                  [:td.env-a]
+                  (html/content (format-numeric patch :env-a))
+                  [:td.env-d]
+                  (html/content (format-numeric patch :env-d))
+                  [:td.env-s]
+                  (html/content (format-numeric patch :env-s))
+                  [:td.env-r]
+                  (html/content (format-numeric patch :env-r))
+
+                  [:td.chorus]
+                  (html/content (str (:chorus (:definition patch))))
+))                  
+
+                                               
   
 (defn -main
   "I don't do a whole lot ... yet."
